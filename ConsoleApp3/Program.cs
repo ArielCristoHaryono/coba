80c49b12f19f92ddd29c5bb7bb79b446c790fe2f
Ariel <Ariel@LAPTOP-95E0E5KT>
2021-12-03 09:02:36 +0700

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nama = 1;
+            while (nama <= 5)
+            {
+
+                Console.Write($"Nama {nama++} :");
+                string Nama = Console.ReadLine();
+                Console.Write("Alamat :");
+                string Rumah = Console.ReadLine();
+
+                //
+                //    Console.WriteLine("==> cari nama: ")
+                //       Int cariNama = ConvertToInt32(Console.ReadLine());
+
+            while (mintaAgain != "y" && mintaAgain != "n")
+            {
+                Console.Write("Coba lagi? (y/n) ");
+                mintaAgain = Console.ReadLine();
+            }
+                if (mintaAgain == "y")
+                {
+                    Console.Write("==> cari nama: ");
+                    cariNama = Convert.ToInt32(Console.ReadLine());
+                    mintaAgain = "";
+                }
+                else if (mintaAgain == "n")
+                {
+                    statusProgram = 1;
+                }
+            }
+                
+
+                //if (namaKe = namaKe++) // namanya sudah ada 
+                //{
+                //    Console.WriteLine("Nama sudah ada");
+                //}
+
+                //Console.WriteLine("==> cari nama: ");
+                //    if () // nama yang dicari tidak ada 
+                //{
+                //    Console.WriteLine("Nama tidak ditemukan");
+                //}
+                //Console.WriteLine("cari lagi? (y/n)")
+                //    string cari = Console.ReadLine();
+                //if (y) // ulangi                console.writeline("==> cari nama: ")
+                //    if () // nama yang dicari tidak ada 
+                //{
+                //    console.writeline("nama tidak ditemukan");
+                //}
+                //console.writeline("cari lagi? (y/n)")
+                //    string cari = console.readline();
+                //if (y) // ulangi
+                //if (key == n) // stop
+                //{
+                //    break;
+                //}
+
+            }
+        }
+    }
