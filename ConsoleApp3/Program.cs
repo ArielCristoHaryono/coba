094c4463bf020c5d5953e8fda84eb93cbc7fe323
ArielCristoHaryono <aharyono01@student.ciputra.ac.id>
2021-12-03 09:54:20 +0700

    tersera

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp3
         static void Main(string[] args)
         {
             int nama = 1;
-            while (nama <= 5)
+          
             {
 
                 Console.Write($"Nama {nama++} :");
